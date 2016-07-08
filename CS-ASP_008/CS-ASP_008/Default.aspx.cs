1e776a57e9fc80781c0889da5ab6f50d314931dc
Goitia <rgoitia@tree.com>
2016-07-08 10:34:22 -0400

    First commit of files for CS-ASP_008

---
+++ CS-ASP_008/CS-ASP_008/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_008
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            /*
+            int  i;
+            i = "3";
+            */
+
+            /*
+            int i = 2000000000;
+            long j = i;
+            */
+
+            /*
+             * Downcast
+            long i = 2000000000;
+            int j = (int)i;
+            */
+
+            /*
+            double k = 2.5;
+            int j = (int)k;
+            */
+
+            //resultLabel.Text = j;
+            //cannot implicitly convert int to string
+            //resultLabel.Text = (string)j;
+            //cannot explicitly convert int to string. use helper method
+            //resultLabel.Text = j.ToString();
+
+            string i = inputTextBox.Text;
+            int j = int.Parse(i);
+            int k = j + 1;
+            resultLabel.Text = k.ToString();
+
+        }
+    }
+}
